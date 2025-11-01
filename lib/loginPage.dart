e03decad75cad0f9b98728c538cf20d1bf3ba1be
Iv8-9 <isvanna.010975@gmail.com>
2025-11-01 17:04:37 -0600

    Login y Registro

---
+++ lib/loginPage.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:quickalert/quickalert.dart';
+import 'package:http/http.dart' as http;
+
+class loginPage extends StatefulWidget {
+  const loginPage({super.key});
+
+  @override
+  State<loginPage> createState() => _loginPageState();
+}
