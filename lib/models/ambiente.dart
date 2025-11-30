daee0fccb6ee0e5f14300082f97ff68911395fb8
LutcCode <2523160015@e.uttecamac.edu.mx>
2025-11-29 20:38:58 -0600

    Calendario Registro

--- lib/models/ambiente.dart
+++ lib/models/ambiente.dart
@@ -1,8 +1,3 @@
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:constelacion/models/libroModel.dart';
-
 class Ambiente {
   static String urlServer = "http://127.0.0.1:8000";
   //static String urlServer = "https://bookcraft-production.up.railway.app";
